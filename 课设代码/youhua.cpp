369de34fa2a047c202ff2458fe93eb351bbe5ab6
计算机1804-20184371-苏奇焱 <65658977+1051520740@users.noreply.github.com>
2020-07-02 20:24:53 +0800

    youhua

--- 课设代码/youhua.cpp
+++ 课设代码/youhua.cpp
@@ -8,6 +8,18 @@
 
 using namespace std;
 
+struct SYNBLTerm//符号表
+{
+	char content[30];
+	string type;
+	char cat[3];
+	int addr;
+	char active;
+	SYNBLTerm* next;
+};
+SYNBLTerm* SYNBLHead[50];
+SYNBLTerm* SYNBLp2;
+
 struct QtTerm//四元式结构体
 {
 	string first;
@@ -30,22 +42,25 @@ struct DAGnode//DAG图的节点结构体
 	int ChildLeft = -1;		//左后继
 	int ChildRight = -1;	//右后继
 	vector<string>SecondMark;//附加标记
-	string FirstMark;		//主标记
+	string FirstMark;	//主标记
+	string s[4];
 };
 vector<DAGnode>DAG;
 
 double StringToDouble(string s)
 {
-	double n = atof(s.c_str());
+	stringstream ss;
+	double n;
+	ss << s;
+	ss >> n;
 	return n;
 }
 
 string DoubleToString(double n)
 {
-	char buffer[20];
-	sprintf_s(buffer, "%f", n);
-	string s = buffer;
-	return s;
+	stringstream ss;
+	ss << n;
+	return ss.str();
 }
 
 bool IsNumber(string s)//判断是否为数字
@@ -68,7 +83,7 @@ int IsDefined_FS(string s)//查主标志和附加标志里是否被定义过
 			return n;
 		else//查DAG附加标记
 		{
-			for (int i = DAG[n].SecondMark.size() - 1; i >= 0; i++)
+			for (int i = DAG[n].SecondMark.size() - 1; i >= 0; i--)
 			{
 				if (DAG[n].SecondMark[i] == s)
 					return n;
@@ -78,11 +93,11 @@ int IsDefined_FS(string s)//查主标志和附加标志里是否被定义过
 	return -1;
 }
 
-void IsDefined_S(string s)//只查附加标志,有就删去，无则继续
+void IsDefined_S(string s,int n)//除了被插入附加标记的那个节点，其他节点只查附加标志,有就删去，无则继续
 {
-	for (int n = DAG.size() - 1; n >= 0; n--)
+	for ( n = n - 1; n >= 0; n--)
 	{
-		for (int i = DAG[n].SecondMark.size() - 1; i >= 0; i++)
+		for (int i = DAG[n].SecondMark.size() - 1; i >= 0; i--)
 		{
 			if (DAG[n].SecondMark[i] == s)
 				DAG[n].SecondMark.erase(DAG[n].SecondMark.begin()+i);
@@ -107,20 +122,34 @@ double Compute(double a,string s,double b)
 	if (s == "<=")return a <= b;
 }
 
-int InSymbleTerm(string s)//判断是否在符号表中
+bool InSymbleTerm(string s)//判断是否在符号表中
 {
-
+	if (s[0] == '-')
+		return false;
+	return true;
 }
 
-void SecondToFirst()
+void SecondToFirst()//把主标记换成非临时变量
 {
 	for (int i = 0; i < DAG.size(); i++)
 	{
-
+		if (!InSymbleTerm(DAG[i].FirstMark))//主标记不在符号表
+		{
+			for (int j = 0; j < DAG[i].SecondMark.size(); j++)
+			{
+				if (InSymbleTerm(DAG[i].SecondMark[j]))//查附标记是否有在符号表里的,有则替换
+				{
+					string temp= DAG[i].SecondMark[j];
+					DAG[i].SecondMark[j] = DAG[i].FirstMark;
+					DAG[i].FirstMark = temp;
+					break;
+				}
+			}
+		}
 	}
 }
 
-void DAGyouhua(int begin,int end)
+void DAGyouhua(int begin,int end)//优化建图
 {
 	for (int i=begin; i <= end; i++)
 	{
@@ -137,7 +166,7 @@ void DAGyouhua(int begin,int end)
 				dagnode.SecondMark.push_back(Qt[i].fourth);
 				DAG.push_back(dagnode);
 			}
-			IsDefined_S(Qt[i].fourth);
+			IsDefined_S(Qt[i].fourth, IsDefined_FS(Qt[i].second));
 		}
 		else if (Qt[i].first == "*" || Qt[i].first == "/" || Qt[i].first == "%" || Qt[i].first == "||" || Qt[i].first == "&&"
 			|| Qt[i].first == "==" || Qt[i].first == "!=" || Qt[i].first == ">=" || Qt[i].first == ">" || Qt[i].first == "<="
@@ -159,11 +188,11 @@ void DAGyouhua(int begin,int end)
 					dagnode.SecondMark.push_back(Qt[i].fourth);
 					DAG.push_back(dagnode);
 				}
-			    IsDefined_S(Qt[i].fourth);//如果forth在之前的附加标记，则删去
+				IsDefined_S(Qt[i].fourth, IsDefined_FS(Qt[i].second));//如果forth在之前的附加标记，则删去
 			}
 			else 
 			{
-				int left = -2, right = -2, flag;
+				int left = -2, right = -2, flag = 0;
 				if (IsDefined_FS(Qt[i].second) != -1)
 				{
 					left = IsDefined_FS(Qt[i].second);
@@ -174,7 +203,7 @@ void DAGyouhua(int begin,int end)
 					dagnode.FirstMark = Qt[i].second;
 					DAG.push_back(dagnode);
 				}
-				if (IsDefined_FS(Qt[i].third) != -1)
+				if (IsDefined_FS(Qt[i].third) != -1)//右孩子被定义过
 				{
 					right = IsDefined_FS(Qt[i].third);
 				}
@@ -202,7 +231,7 @@ void DAGyouhua(int begin,int end)
 					dagnode.ChildRight = IsDefined_FS(Qt[i].third);
 					DAG.push_back(dagnode);
 				}
-				IsDefined_S(Qt[i].fourth);//forth之前附加标志定义过，则删除之
+				IsDefined_S(Qt[i].fourth, IsDefined_FS(Qt[i].second));//forth之前附加标志定义过，则删除之
 			}
 		}
 		else if (Qt[i].first == "!" || (Qt[i].first == "-" && Qt[i].third == "_"))//单目运算符
@@ -235,20 +264,92 @@ void DAGyouhua(int begin,int end)
 				dagnode.operate = Qt[i].first;
 				DAG.push_back(dagnode);
 			}
-			IsDefined_S(Qt[i].fourth);
+			IsDefined_S(Qt[i].fourth, IsDefined_FS(Qt[i].second));
 		}
-		else if (Qt[i].first == "hanshu" || Qt[i].first == "re" || Qt[i].first == "diaoyong" || Qt[i].first == "xingcan"
-			|| Qt[i].first == "canshu" || Qt[i].first == "END" || Qt[i].first == "temp")
+		else if (Qt[i].first == "hanshu" || Qt[i].first == "re" || Qt[i].first == "diaoyong" || Qt[i].first == "wh" || 
+		     Qt[i].first == "xingcan" || Qt[i].first == "canshu" || Qt[i].first == "END"|| Qt[i].first == "if"||
+			 Qt[i].first == "el" || Qt[i].first == "do" || Qt[i].first == "we" || Qt[i].first == "ie" || Qt[i].first == "temp")
 		{
-			
+			DAGnode dagnode;
+			dagnode.s[0] = Qt[i].first;
+			dagnode.s[1] = Qt[i].second;
+			dagnode.s[2] = Qt[i].third;
+			dagnode.s[3] = Qt[i].fourth;
+			DAG.push_back(dagnode);
 		}
 	}
-
+	SecondToFirst();//主标记换成非临时变量
 }
 
-void DAGoutput()
+void DAGoutput()//重组四元式
 {
-
+	for (int j = 0; j < DAG.size(); j++)
+	{
+		if (DAG[j].SecondMark.size() != 0 && DAG[j].ChildLeft == -1 && DAG[j].ChildRight == -1)//带有附加标记的叶节点
+		{
+			if (InSymbleTerm(DAG[j].FirstMark))//主标记为非临时变量
+			{
+				for (int i = 0; i < DAG[j].SecondMark.size(); i++)//为非临时变量，则生成赋值四元式
+				{
+					if (InSymbleTerm(DAG[j].SecondMark[i]))
+					{
+						QtTerm newqt;
+						newqt.first = "=";
+						newqt.second = DAG[j].FirstMark;
+						newqt.third = "_";
+						newqt.fourth = DAG[j].SecondMark[i];
+						NewQt.push_back(newqt);
+					}
+				}
+			}
+		}
+		else if(DAG[j].SecondMark.size()!=0 && (DAG[j].ChildLeft!=-1 || DAG[j].ChildRight != -1))//带附加标记，并且不为叶节点
+		{
+			if (DAG[j].ChildLeft != -1 && DAG[j].ChildRight != -1)//两个后继
+			{
+				QtTerm newqt;
+				newqt.first = DAG[j].operate;
+				newqt.second = DAG[DAG[j].ChildLeft].FirstMark;
+				newqt.third = DAG[DAG[j].ChildRight].FirstMark;
+				newqt.fourth = DAG[j].FirstMark;
+				NewQt.push_back(newqt);
+			}
+			else if (DAG[j].ChildLeft != -1 && DAG[j].ChildRight == -1)//只有左孩子
+			{
+				QtTerm newqt;
+				newqt.first = DAG[j].operate;
+				newqt.second = DAG[DAG[j].ChildLeft].FirstMark;
+				newqt.third = "_";
+				newqt.fourth = DAG[j].FirstMark;
+				NewQt.push_back(newqt);
+			}
+			if (InSymbleTerm(DAG[j].FirstMark))//主标记为非临时变量
+			{
+				for (int i = 0; i < DAG[j].SecondMark.size(); i++)//附加标志为非临时变量，则生成赋值四元式
+				{
+					if (InSymbleTerm(DAG[j].SecondMark[i]))//在附加标记里如果是非临时变量，则产生赋值四元式
+					{
+						QtTerm newqt;
+						newqt.first = "=";
+						newqt.second = DAG[j].FirstMark;
+						newqt.third = "_";
+						newqt.fourth = DAG[j].SecondMark[i];
+						NewQt.push_back(newqt);
+					}
+				}
+			}
+		}
+		else if(!DAG[j].s[0].empty())
+		{
+			QtTerm newqt;
+			newqt.first = DAG[j].s[0];
+			newqt.second = DAG[j].s[1];
+			newqt.third = DAG[j].s[2];
+			newqt.fourth = DAG[j].s[3];
+			NewQt.push_back(newqt);
+		}
+	}
+	DAG.clear();
 }
 
 void DivideBaseblock()//划分基本块
@@ -259,9 +360,33 @@ void DivideBaseblock()//划分基本块
 		if (Qt[end].first == "if" || Qt[end].first == "el" || Qt[end].first == "ie" || Qt[end].first == "wh" || Qt[end].first == "do" || Qt[end].first == "we")
 		{
 			DAGyouhua(begin, end);
+			DAGoutput();
 			begin = end+1;
 		}
 	}
 	if (begin < end)
+	{
 		DAGyouhua(begin, end - 1);
+		DAGoutput();
+	}
+}
+
+int main()
+{
+	string s[4];
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			cin >> s[i];
+		}
+		QtTerm qt;
+		qt.first = s[0]; qt.second = s[1]; qt.third = s[2]; qt.fourth = s[3];
+		Qt.push_back(qt);
+	}
+	DivideBaseblock();
+	for (int i = 0; i < NewQt.size(); i++)
+	{
+		cout << NewQt[i].first << '\t' << NewQt[i].second << '\t' << NewQt[i].third << '\t' << NewQt[i].fourth<<endl;
+	}
 }
